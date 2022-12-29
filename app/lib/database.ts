f25a1697a7cc248db2c38b6703c5d250e5406df3
Hunter Kalinoski <hunterkalinoski@gmail.com>
2022-12-29 14:23:28 -0500

    created users collection in firestore UI functionality to create new users / sign in to custom user

--- app/lib/database.ts
+++ app/lib/database.ts
@@ -1,7 +1,30 @@
-import { getFirestore } from "firebase/firestore";
+import { addDoc, collection, getDocs, getFirestore } from "firebase/firestore";
 import firebaseApp from "@lib/firebase";
 
 // Initialize Cloud Firestore
-const db = getFirestore(firebaseApp);
+export const db = getFirestore(firebaseApp);
 
-export default db;
+// create a new user document in the users collection
+// params display_name and email are strings to be associated with the user's database entry
+// returns true if successful, false if an error occurred
+export const createUserDocument = async (display_name: string, email: string): Promise<boolean> => {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      display_name: display_name,
+      email: email,
+    });
+    console.log("Document written with ID: ", docRef.id);
+    return true;
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    return false;
+  }
+};
+
+// log out a list of all currently signed up users
+export const logUsersCollection = async () => {
+  const querySnapshot = await getDocs(collection(db, "users"));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+};
--- app/page.tsx
+++ app/page.tsx
@@ -2,40 +2,28 @@
 
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { createNewUser, logInUser, logOutUser } from "@lib/auth";
-import db from "@lib/database";
+import { db, createUserDocument, logUsersCollection } from "@lib/database";
 import HelloWorld from "@components/HelloWorld";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "@lib/userContext";
 
 export default function Home() {
   const user = useContext(UserContext);
-  const createPost = async () => {
-    try {
-      const docRef = await addDoc(collection(db, "users"), {
-        first: "John",
-        middle: "A",
-        last: "Smith",
-        born: 2000,
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
+  const [displayName, setDisplayName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  // create both an auth user and a user database entry
+  const signUp = async (display_name: string, email: string, password: string) => {
+    console.log(`display_name: ${display_name}, email: ${email}, password: ${password}`);
+    const user = await createNewUser(email, password);
+    if (user) {
+      createUserDocument(display_name, email);
     }
   };
 
-  const logPosts = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
-  };
-
-  const signUp = async () => {
-    const user = await createNewUser("test@gmail.com", "password");
-    return user;
-  };
-  const signIn = async () => {
-    const user = await logInUser("test@gmail.com", "password");
+  const signIn = async (email: string, password: string) => {
+    const user = await logInUser(email, password);
     return user;
   };
 
@@ -52,18 +40,37 @@ export default function Home() {
       <HelloWorld />
       <HelloWorld />
       <HelloWorld />
-      <button onClick={createPost} className="border border-black">
-        Create Post
-      </button>
-      <button onClick={logPosts} className="border border-black">
+      <label>Username: </label>
+      <input
+        type="text"
+        name="display_name"
+        onChange={(e) => setDisplayName(e.currentTarget.value)}
+        className="border border-black"
+      />
+      <label>Email: </label>
+      <input
+        type="text"
+        name="email"
+        onChange={(e) => setEmail(e.currentTarget.value)}
+        className="border border-black"
+      />
+      <label>Password</label>
+      <input
+        type="text"
+        name="password"
+        onChange={(e) => setPassword(e.currentTarget.value)}
+        className="border border-black"
+      />
+
+      <button onClick={logUsersCollection} className="border border-black">
         Log Posts
       </button>
       <p>{"current user: " + user?.email}</p>
 
-      <button onClick={signUp} className="border border-black">
+      <button onClick={() => signUp(displayName, email, password)} className="border border-black">
         Sign Up
       </button>
-      <button onClick={signIn} className="border border-black">
+      <button onClick={() => signIn(email, password)} className="border border-black">
         Sign In
       </button>
       <button onClick={signOut} className="border border-black">
