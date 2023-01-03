7be71af766b4cbb19781e7c5d22715fdc747e669
Hunter Kalinoski <hunterkalinoski@gmail.com>
2023-01-03 15:37:10 -0500

    main page loading spinner and fixed navbar loadng

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,27 +3,31 @@
 import { useAuthState } from "@hooks/useAuthState";
 import { logOutUser } from "@lib/auth";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar({}) {
   const [auth] = useAuthState();
+  const [authFetched, setAuthFetched] = useState(0);
 
   return (
     <>
-      <div className="flex flex-row w-full justify-between bg-slate-900 text-gray-200 p-5 items-center">
-        <Link href="/" prefetch className="hover:underline">
+      <div className="flex flex-row w-full justify-between bg-slate-900 text-gray-200 p-5 items-center h-20">
+        <Link href="/" className="hover:underline">
           <h2>Next-Firebase Todos</h2>
         </Link>
         {auth ? (
           <button onClick={logOutUser}>Sign Out</button>
-        ) : (
+        ) : authFetched ? (
           <span className="flex flex-row gap-5">
-            <Link href="/signin" prefetch>
+            <Link href="/signin">
               <button>Sign In</button>
             </Link>
-            <Link href="/signup" prefetch>
+            <Link href="/signup">
               <button>Sign Up</button>
             </Link>
           </span>
+        ) : (
+          <></>
         )}
       </div>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 
 import Link from "next/link";
 import { useAuthState } from "@hooks/useAuthState";
-import { logOutUser } from "@lib/auth";
 import { deleteTodo, getCurrentUserDoc, getCurrentUserTodos } from "@lib/database";
 import { useEffect, useState } from "react";
 import { DocumentData } from "firebase/firestore";
+import { TailSpin } from "react-loader-spinner";
 
 export default function Page() {
   const [auth] = useAuthState();
+  const [authFetched, setAuthFetched] = useState(0);
   const [userDoc, setUserDoc] = useState<DocumentData | null>(null);
   const [todos, setTodos] = useState<DocumentData[] | null>(null);
 
@@ -19,6 +20,7 @@ export default function Page() {
     getCurrentUserTodos().then((data) => {
       setTodos(data);
     });
+    setAuthFetched(authFetched + 1);
   }, [auth]);
 
   const finishTodo = async (todoId: string) => {
@@ -33,10 +35,10 @@ export default function Page() {
 
   return (
     <>
-      {auth ? (
+      {auth && userDoc ? (
         <>
           <h2>Hello {userDoc?.display_name}</h2>
-          <Link href="/create" prefetch>
+          <Link href="/create">
             <button>Create a new Todo</button>
           </Link>
           <h1 className="mt-20">Your Todos:</h1>
@@ -75,20 +77,33 @@ export default function Page() {
           </ul>
           <p className="pb-10"></p>
         </>
-      ) : (
+      ) : authFetched > 2 ? (
         <>
           <h1>Welcome to Next-Firebase todos</h1>
           <p>This is a todo app built with Nextjs and Firebase(Auth and Firestore)</p>
           <span className="flex flex-row gap-2 items-center">
-            <Link href="/signup" prefetch>
+            <Link href="/signup">
               <button>Sign up</button>
             </Link>
             <p>or</p>
-            <Link href="/signin" prefetch>
+            <Link href="/signin">
               <button>Sign in</button>
             </Link>
             <p>to get started</p>
           </span>
+        </>
+      ) : (
+        <>
+          <TailSpin
+            height="80"
+            width="80"
+            color="#334155"
+            ariaLabel="tail-spin-loading"
+            radius="1"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
         </>
       )}
     </>
